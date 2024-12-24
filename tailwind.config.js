1487868837bd75218027994bb21fae1133835df2
TranDanh1122 <trandanh14042000@gmail.com>
2024-12-24 16:36:34 +0700

    layout + setup state complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark_navy": "#1A2A33",
+        "semi_navy": "#1F3641",
+        "sliver": "#A8BFC9",
+        "sliver_hover": "#DBE8ED",
+        "light_blue": "#31C3BD",
+        "light_blue_hover": "#65E9E4",
+        "light_yellow": "#F2B137",
+        "light_yellow_hover": "#FFC860"
+      }
+    },
   },
   plugins: [],
 }
